a6b61e5abe74db40bb82e375eba790cc216a23c9
ujhwang <uhwang3@gatech.edu>
2023-04-04 01:49:00 -0400

    Update train_script.py

--- train_scripts/train_script.py
+++ train_scripts/train_script.py
@@ -75,22 +75,21 @@ def train(device, net, dataloader, val_loader, args, logger, experiment):
             self.lam = lam
 
         def forward(self, output_left, output_right, label):
-            # convert label from -1/1 to 0/1 and cast to torch tensor
-            # label_binary = (label + 1) / 2
-            # #label_binary = torch.tensor(label_binary, dtype=torch.float32, device=output_left.device)
+            #convert label from -1/1 to 0/1 and cast to torch tensor
+            label_binary = (label + 1) / 2
 
-            # # calculate binary cross entropy loss for both outputs
-            # loss1 = F.binary_cross_entropy(output_left, label_binary)
-            # loss2 = F.binary_cross_entropy(output_right, 1 - label_binary)
-            # binary_loss = loss1 + loss2
+            # calculate binary cross entropy loss for both outputs
+            loss1 = F.binary_cross_entropy(output_left, label_binary)
+            loss2 = F.binary_cross_entropy(output_right, 1 - label_binary)
+            binary_loss = loss1 + loss2
             
             # calculate margin ranking loss
             ranking_loss = F.relu(self.margin - (output_left - output_right) * label)**2
             
             # return the mean of the losses over the batch
-            return torch.mean(self.lam * ranking_loss)
+            return torch.mean(binary_loss + self.lam * ranking_loss)
     
-    criterion = CustomJointLoss()
+    criterion = CustomJointLoss(margin = 0.2, lam = 1)
 
 
 
