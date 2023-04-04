52472b837bd35127f684499eb94dd976dd6b36de
ujhwang <uhwang3@gatech.edu>
2023-04-04 01:12:09 -0400

    Update train_script.py

--- train_scripts/train_script.py
+++ train_scripts/train_script.py
@@ -54,9 +54,9 @@ def train(device, net, dataloader, val_loader, args, logger, experiment):
             forward_dict = net(input_left,input_right)
             output_rank_left, output_rank_right =  forward_dict['left']['output'], forward_dict['right']['output']
             if args.attribute == 'all':
-                loss = compute_multiple_ranking_loss(output_rank_left, output_rank_right, label, custom_joint_loss, attribute)
+                loss = compute_multiple_ranking_loss(output_rank_left, output_rank_right, label, criterion, attribute)
             else:
-                loss = compute_ranking_loss(output_rank_left, output_rank_right, label, custom_joint_loss)
+                loss = compute_ranking_loss(output_rank_left, output_rank_right, label, criterion)
 
             return  { 'loss':loss.item(),
                 'rank_left': output_rank_left,
@@ -67,22 +67,28 @@ def train(device, net, dataloader, val_loader, args, logger, experiment):
     # Define model, loss, optimizer and scheduler
     net = net.to(device)
     # rank_crit = nn.MarginRankingLoss(reduction='mean', margin=1)
+    
+    class CustomJointLoss(nn.Module):
+        def __init__(self):
+            super(CustomJointLoss, self).__init__()
 
-    def custom_joint_loss(output_left, output_right, label, margin=0.2, lam = 1):
-        # convert label from -1/1 to 0/1 and cast to torch tensor
-        label_binary = (label + 1) / 2
-        #label_binary = torch.tensor(label_binary, dtype=torch.float32, device=output_left.device)
+        def forward(output_left, output_right, label, margin=0.2, lam = 1):
+            # convert label from -1/1 to 0/1 and cast to torch tensor
+            label_binary = (label + 1) / 2
+            #label_binary = torch.tensor(label_binary, dtype=torch.float32, device=output_left.device)
 
-        # calculate binary cross entropy loss for both outputs
-        loss1 = F.binary_cross_entropy(output_left, label_binary)
-        loss2 = F.binary_cross_entropy(output_right, 1 - label_binary)
-        binary_loss = loss1 + loss2
-        
-        # calculate margin ranking loss
-        ranking_loss = F.relu(margin - (output_left - output_right) * label)**2
-        
-        # return the mean of the losses over the batch
-        return torch.mean(binary_loss + lam * ranking_loss)
+            # calculate binary cross entropy loss for both outputs
+            loss1 = F.binary_cross_entropy(output_left, label_binary)
+            loss2 = F.binary_cross_entropy(output_right, 1 - label_binary)
+            binary_loss = loss1 + loss2
+            
+            # calculate margin ranking loss
+            ranking_loss = F.relu(margin - (output_left - output_right) * label)**2
+            
+            # return the mean of the losses over the batch
+            return torch.mean(binary_loss + lam * ranking_loss)
+    
+    criterion = CustomJointLoss()
 
 
 
